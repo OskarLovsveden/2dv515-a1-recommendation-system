fd0d9bc68495c5756d6c003b2da6a5d35de26f9d
OskarLovsveden <oskarlovsveden@hotmail.com>
2021-11-11 19:08:25 +0100

    adding recommendation endpoints

--- pages/api/recommendation.js
+++ pages/api/recommendation.js
@@ -1,1 +1,16 @@
-// recommendation routes here...
+export default function userHandler(req, res) {
+    const {
+        body: { user },
+        method,
+    } = req
+
+    switch (method) {
+        case 'POST':
+            // handle recommendations data
+            res.status(200).json({ message: 'OK' })
+            break
+        default:
+            res.setHeader('Allow', ['POST'])
+            res.status(405).end(`Method ${method} Not Allowed`)
+    }
+}
